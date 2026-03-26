56a20a7695f02a46ec63be67742013279cb518e3
fpachilla <fpachlaboral@gmail.com>
2026-03-26 09:36:12 -0300

    Se agrega función agregar_tarea

--- gestion_tareas.py
+++ gestion_tareas.py
@@ -1,3 +1,5 @@
+from tareas import Tarea
+
 def eliminar_tarea(lista, id):
 
     tareaencontrada = False
@@ -14,3 +16,26 @@ def eliminar_tarea(lista, id):
 
     except ValueError:
         print("Debe ingresar un numero entero")
+
+def agregar_tarea(lista):
+
+    titulo_tarea = input(f'Ingrese el titulo de la tarea: ')
+    descripcion_tarea = input(f'Ingrese la descripcion de la tarea: ')
+    while True:
+        try:
+            prioridad_tarea = int(input("Ingrese prioridad (1-3): "))
+
+            if 1 <= prioridad_tarea <= 3:
+                break
+            else:
+                print("La prioridad debe estar entre 1 y 3.")
+
+        except ValueError:
+            print("Debe ingresar un número entero.")
+    estado_tarea = input(f'Ingrese la estado de la tarea (solo puede ser "pendiente" o "hecha"): ')
+    while (estado_tarea != "pendiente" and estado_tarea != "hecha"):
+        estado_tarea = input(
+            f'Solamente se puede ingresar "pendiente" o "hecha". Por favor ingrese una de esas opciones: ')
+    tareaIngresada = Tarea(titulo_tarea, descripcion_tarea, prioridad_tarea, estado_tarea)
+
+    lista.append(tareaIngresada)
